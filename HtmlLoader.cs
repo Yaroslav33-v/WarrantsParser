e1b2ff684e75ea1ebc68e3d1f219a12920314b64
Yaroslav <Yaroslav@LAPTOP-E3KDM2J0>
2025-07-10 15:29:27 +0300

    Изменены имена переменных на подходящие

--- HtmlLoader.cs
+++ HtmlLoader.cs
@@ -8,24 +8,24 @@ namespace WarrantyParser
     internal class HtmlLoader
     {
         private static Logger _logger = LogManager.GetCurrentClassLogger();
-        readonly HttpClient Client;
-        readonly string Url;
+        readonly HttpClient сlient;
+        readonly string url;
 
         public HtmlLoader()
         {
-            Client = new HttpClient();
-            Client.DefaultRequestHeaders.Add("User-Agent", "C# App");
-            Url = "https://www.wienerborse.at/en/warrants/?c10952-page={CurrentPage}&per-page=50";
+            сlient = new HttpClient();
+            сlient.DefaultRequestHeaders.Add("User-Agent", "C# App");
+            url = "https://www.wienerborse.at/en/warrants/?c10952-page={CurrentPage}&per-page=50";
         }
 
         public async Task<string> GetSourceByPage(int currentPage) // Получение данных со страницы
         {
             _logger.Trace($"Загрузка страницы {currentPage}");
-            string currentUrl = Url.Replace("{CurrentPage}", currentPage.ToString());
+            string currentUrl = url.Replace("{CurrentPage}", currentPage.ToString());
 
             try
             {
-                HttpResponseMessage response = await Client.GetAsync(currentUrl);
+                HttpResponseMessage response = await сlient.GetAsync(currentUrl);
                 if (response != null && response.StatusCode == System.Net.HttpStatusCode.OK)
                 {
                     _logger.Debug($"Успешно загружена страница {currentPage}");
